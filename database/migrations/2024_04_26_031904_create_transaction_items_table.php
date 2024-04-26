22786faa26edfbb4651e6d6a6e67149b47e46238
Zidane Abrisam Latif <zidaneabrisam1428@gmail.com>
2024-04-26 11:11:48 +0700

    first commit

---
+++ database/migrations/2024_04_26_031904_create_transaction_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Product;
+use App\Models\Transaction;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaction_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignFor(Transaction::class)->references('id')->on('transactions')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignFor(Product::class)->references('id')->on('products')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignFor(User::class)->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaction_items');
+    }
+};
